d62678246801cc2353238dbd66b146971104fec3
ZonaryTitan <106260496+TitanProgrammer4480@users.noreply.github.com>
2024-11-08 10:37:21 +0000

    added player class/main/full stats

--- wynncraft-wrapper/main.py
+++ wynncraft-wrapper/main.py
@@ -1,4 +1,16 @@
+import requests as r
 
-class Wynncraft:
-    def __init__(self):
-      pass
+class Player:
+    def __init__(self, name, uuid=""):
+      self.name = name
+      self.uuid = uuid
+      self.url = "https://api.wynncraft.com/v3/player"
+      self.data = {}
+
+    def main_stats(self):
+        self.data = r.get(self.url + "/" + self.name + "/" + self.uuid)
+        return self.data
+
+    def full_stats(self):
+        self.data = r.get(self.url + "/" + self.name + "/" + self.uuid + "?fullResult")
+        return self.data
