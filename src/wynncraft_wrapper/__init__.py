6de3b242c50d56ebb48eecc7c78219578a19661f
ZonaryTitan <106260496+TitanProgrammer4480@users.noreply.github.com>
2024-12-12 16:17:36 +0000

    added init.py

---
+++ src/wynncraft_wrapper/__init__.py
@@ -0,0 +1,7 @@
+from .guild import Guild
+from .item import Item
+from .leaderboard import Leaderboard
+from .news import News
+from .player import Player
+
+__all__ = ["Guild", "Item", "Leaderboard", "News", "Player"]
